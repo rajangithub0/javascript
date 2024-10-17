2cabf12377b517f5f2594139ef4a3c3770fa5d21
Rajan <rajan.rana6@gmail.com>
2024-10-17 14:28:24 +0530

    nested loop concept and spril order

---
+++ Session-5-nested loop/rightside.js
@@ -0,0 +1,9 @@
+for(var row=1;row<=4;row++){
+    var ptr="";
+    for(var col=1;col<=row;col++){
+        //prt+="#"// if  print the star;
+        //ptr+=col // if print the number;
+        //ptr+=row; // if print the number
+    }
+    console.log(ptr);
+}
