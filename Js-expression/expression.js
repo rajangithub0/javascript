94d4d88d2a3df147b52658685107a5fa73dc7268
Rajan <rajan.rana6@gmail.com>
2024-10-31 22:02:22 +0530

    expression of js

---
+++ Js-expression/expression.js
@@ -0,0 +1,12 @@
+const regex = new RegExp(/^r...n$/);
+console.log(regex.test("rarfn"));
+
+// const nota=new RegExp(/[^a-i]/)
+// console.log(nota.test("koot"));
+
+// const kpl= new RegExp(/[^K...../d$]/)
+// console.log(kpl.test("kpl123RR"));
+
+// const demo= new RegExp(/[a-z]$/);
+// console.log(demo.test('monika'));
+
